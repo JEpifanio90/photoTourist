1c94b855789f6ee35697ad8474627be40ff26fad
Jose Epifanio <jose.epifanio90@gmail.com>
2017-11-04 00:22:12 -0600

    Add more seeds to play with

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,38 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-city = City.new
-city.name = 'Chicago'
-city.save
+City.create(
+    [
+        { name: 'Chicago' },
+        { name: 'Washington' },
+        { name: 'New York city' },
+        { name: 'Phoenix' },
+        { name: 'San Francisco' },
+        { name: 'Boston' },
+        { name: 'Detroit' },
+        { name: 'Seattle' },
+        { name: 'Baltimore' },
+        { name: 'Portland' },
+        { name: 'Boise' },
+        { name: 'San Diego' },
+        { name: 'Austin' }
+    ]
+)
 
-city.name = 'Nuevo Laredo'
-city.save
-
-city.name = 'San Antonio'
-city.save
-
-state = State.new
-state.name = 'Michigan'
-state.save
-
-state.name = 'California'
-state.save
-
-state.name = 'Nueva York'
-state.save
+State.create(
+    [
+        { name: 'Texas' },
+        { name: 'California' },
+        { name: 'Indiana' },
+        { name: 'Virgina' },
+        { name: 'Florida' },
+        { name: 'Colorado' },
+        { name: 'Ohio' },
+        { name: 'Georgia' },
+        { name: 'Arizona' },
+        { name: 'Alabama' },
+        { name: 'Illinois' },
+        { name: 'Tennessee' },
+        { name: 'Michigan' }
+    ]
+)
